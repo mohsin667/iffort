d5b28f93b2d432b008c4fa01a4b972a5b1b90fde
Mohsin Ansari <ansarimohsin667@gmail.com>
2020-10-14 08:51:24 +0530

    working on website

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     $(".menu").on("click",function(){
         $("nav").toggleClass("open")
+        $("body").toggleClass("hidden")
     })
    
    
@@ -20,63 +21,42 @@ $(document).ready(function () {
 
     })
 
-    $(".subscription-slider").slick({
-        fade: true,
-    });
-    // let slickSlider = {
-    //     productCarousel: $('.product-slider'),
-    //     productCarouselNav: $('.slider-nav'),
-    //     relatedItemsCarousel: $(".review-slider"),
+    $('.f-a-section .artcile-slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
 
-    //     slick: function (el, slidesToShow, slidesToScroll, arrows, fade, asNavFor, dots, centerMode, focusOnSelect, infinite) {
-
-    //         el.slick({
-    //             slidesToShow: slidesToShow,
-    //             slidesToScroll: slidesToScroll,
-    //             arrows: arrows,
-    //             fade: fade,
-    //             asNavFor: asNavFor,
-    //             dots: dots,
-    //             centerMode: centerMode,
-    //             focusOnSelect: focusOnSelect,
-    //             infinite: infinite
-    //         });
-
-    //     },
-    //     init: function () {
-    //         slickSlider.slick(slickSlider.productCarousel, 1, 1, false, true, '.slider-nav', false, null, null, true);
-    //         slickSlider.slick(slickSlider.productCarouselNav, 3, 1, false, false, '.product-slider', false, true, true, true);
-    //         let slideW = 1;
-    //         let slidev = 1;
-    //         if ($(window).width() > 800) {
-    //             slideW = 3
-    //         }
-    //         slickSlider.slick(slickSlider.relatedItemsCarousel, slideW, slidev, false, false, null, true, null, null, true);
-    //     }
-    // }
-    // //Functions initialization
-    // slickSlider.init();
-    // function ValidateEmail() {
-
-    // }
-
-    // $(".menu-bar").on("click", function () {
-    //     $(".drop-down").slideToggle();
-    //     $(this).find(".trigger").toggleClass("rotate");
-    // })
-
-    // document.querySelector(".footer form").addEventListener("submit", function (e) {
-    //     e.preventDefault();
-    //     let vals = document.querySelector(".footer form input").value
-    //     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(vals)) {
-    //         $(".error").hide()
-    //         $(".footer form input").val("")
-    //         return (true)
-    //     }
-    //     if (vals == "") {
-    //         $(".error").show()
-    //     }
-    //     $(".error").show()
-    //     return (false)
-    // })
+        responsive: [
+            
+            {
+              breakpoint: 800,
+              settings: {
+                dots: false,
+                slidesToShow: 1.1,
+                slidesToScroll: 1,
+                centerMode: true,
+                focusOnSelect: true
+              }
+            },
+            
+            
+          ]
+      });
+      
+      function mobSlider(){
+          if(window.innerWidth < 800) {
+            $(".t-t-section .artcile-slider").slick({
+                arrows: false,
+                dots: false,
+                infinite: true,
+                slidesToShow: 1.1,
+                slidesToScroll: 1,
+                centerMode: true,
+                focusOnSelect: true
+              });
+          }
+      }
+      mobSlider()
 });
